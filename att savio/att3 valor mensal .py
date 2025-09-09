bbb1ad377f7335490c79180e065020733da55c97
eliasmigueldasilvamaciel-cpu <eliasmigueldasilvamaciel110@gmail.com>
2025-09-09 14:07:27 -0300

    deu

---
+++ att savio/att3 valor mensal .py
@@ -0,0 +1,5 @@
+profissao = input("digite sua profissao: ")
+salario_por_hora = float(input("quanto voce ganha por hora? "))
+valor_semanal = salario_por_hora * 44
+valor_mensal = valor_semanal * 4
+print(f"o valor mensal do seu salario e de {valor_mensal},e vc recebe {valor_semanal}por semana")
