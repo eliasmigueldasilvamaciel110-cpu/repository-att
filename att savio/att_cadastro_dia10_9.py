d083bc0d06e7eb898b26c123c02d9725f04fa76f
eliasmigueldasilvamaciel-cpu <eliasmigueldasilvamaciel110@gmail.com>
2025-09-10 16:35:34 -0300

    certo

---
+++ att savio/att_cadastro_dia10_9.py
@@ -0,0 +1,13 @@
+#crie um cadastro com senha
+usuario = input("diga la seu nome caro usuario: ")
+confirmau = input("confirme seu nome: ")
+if confirmau == usuario:
+    print("usuario correto")
+    senha = input("digite sua senha: ")
+    confirma = input("confirme sua senha: ")
+elif:
+    print("usuario errado seu sacana")
+    senha == confirma
+    print("senha correta")
+else:
+    print("senha errada seu sacana")
--- att savio/att_media_dia10_9.py
+++ att savio/att_media_dia10_9.py
@@ -1,6 +1,6 @@
 a = int(input("digite a primeira nota: "))
-b = int(input("digite a primeira nota: "))
-c = int(input("digite a primeira nota: "))
+b = int(input("digite a segunda nota: "))
+c = int(input("digite a terceira nota: "))
 media = (a + b + c) / 3
 print(f"A média é: {media}")
 if media >= 6:
