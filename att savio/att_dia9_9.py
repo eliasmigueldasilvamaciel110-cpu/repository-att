2b5045ebd2e02eb605ed860f2d34bb3561778084
eliasmigueldasilvamaciel-cpu <eliasmigueldasilvamaciel110@gmail.com>
2025-09-10 15:45:59 -0300

    ??

--- att savio/att_dia9_9.py
+++ att savio/att_dia9_9.py
@@ -4,3 +4,5 @@ b = int(input("fala ai outro valor batola "))
 print(f"antes de trocar os valores:A={a} e B={b}")
 a, b = b, a
 print(f"depois de trocar los valores A={a}e B={b}")
+#lembrar de pedir o savio para retirar o ajudante de codigo
+#lembrar de usar p git add e git commit -m "e escrever algo" dps da push
