5a8a517b245440eb981d1e656cfafa188ae9bafa
eliasmigueldasilvamaciel-cpu <eliasmigueldasilvamaciel110@gmail.com>
2025-09-16 09:42:45 -0300

    loveleeeee

--- att savio/att_cadastro_dia10_9.py
+++ att savio/att_cadastro_dia10_9.py
@@ -1,13 +1,18 @@
 #crie um cadastro com senha
-usuario = input("diga la seu nome caro usuario: ")
-confirmau = input("confirme seu nome: ")
-if confirmau == usuario:
-    print("usuario correto")
-    senha = input("digite sua senha: ")
+while True:
+    usuario = input("diga la seu nome caro usuario: ")
+    confirmau = input("confirme seu nome: ")
+    if confirmau == usuario:
+        print("usuario correto")
+        break  # Sai do loop se o nome estiver correto
+    else:
+        print("usuario errado seu sacana")
+senha = input("digite sua senha: ")
+while True :
     confirma = input("confirme sua senha: ")
-elif:
-    print("usuario errado seu sacana")
-    senha == confirma
-    print("senha correta")
-else:
-    print("senha errada seu sacana")
+    if confirma == senha:
+        print("senha correta")
+        break  # Sai do loop se a senha estiver correta
+    else:
+        print("senha errada seu sacana")
+        #break  serve sempre para sair do loop
